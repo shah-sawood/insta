a226f90d651858765bdb238b283bb318288043e3
shinwaris <zoology.fb@gmail.com>
2022-11-19 16:49:23 +0500

    downloaded bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
+        @endif
+        <div class="col-md-3">
+            <div class="text-center">
+                <img src="{{ asset('images/logo.webp') }}" width="250" class="rounded-circle">
+            </div>
+        </div>
+        <div class="col-md-9">
+            <h1>Insta</h1>
+            <div></div>
+            
         </div>
     </div>
 </div>
